8266eda51775bb41bc4edcb31b917baf2e15adfa
bekzod9936 <bekzodnorkulov9936@gmail.com>
2023-05-26 17:22:58 +0500

    fix: removed

--- src/constants/nav.ts
+++ src/constants/nav.ts
@@ -1,7 +1,6 @@
 export const navlist = [
-	{ title: 'Home', href: '/#main' },
-	{ title: 'About Us', href: '/#about' },
-	{ title: 'Services', href: '/#services' },
-	{ title: 'Contacts', href: '/#contacts' },
-	{ title: 'Testimonials', href: '/#testimonials' },
-]
+  { title: 'Home', href: '/#main' },
+  { title: 'About Us', href: '/#about' },
+  { title: 'Services', href: '/#services' },
+  { title: 'Contacts', href: '/#contacts' },
+];
